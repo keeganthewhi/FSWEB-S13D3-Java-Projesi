8cf8c75d3743c8d9f77456ac2d9860b28532c0f4
keeganthewhi <furkanakyuz1453@gmail.com>
2023-11-09 15:12:22 +0300

    Bitim

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+
+
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*com.workintech.model.Person person = new com.workintech.model.Person("John", "Doe", 20);
+        com.workintech.model.Person person1 = new com.workintech.model.Person("mehmet","metintas",32,true,new String[]{"ing","tr"},"Atasehir");
+
+        System.out.println("Firstname: " + person.getFirstName());
+
+        System.out.println("LastName: " + person.getLastName());
+
+        System.out.println("Age: " + person.getAge());
+    */
+
+        com.workintech.model.Wall wall = new com.workintech.model.Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
     }
 }
+
+
+
+
+
+
+
+
---
+++ src/com/workintech/model/Person.java
@@ -0,0 +1,42 @@
+package com.workintech.model;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    boolean isMarried;
+    String[] langs;
+    String address;
+
+    public Person(String firstName , String lastName ,int age ){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+    public Person(String firstName , String lastName ,int age,boolean isMarried,String[] langs,String address){
+        //this.firstName=firstName;
+        //this.lastName=lastName;
+        //this.age=age;
+        this(firstName,lastName,age);//constructor chaining
+        this.isMarried=isMarried;
+        this.langs=langs;
+        this.address=address;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return  age;
+    }
+    public boolean isTeen(){
+       return age>13 && age<19 ;
+    }
+
+};
+
+
+
+
